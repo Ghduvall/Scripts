0d3e44ebe5bedd451479728e960017fc6681b277
Grant Duvall <grant@mark4tech.com>
2021-06-03 19:33:16 -0500

    Initial commit

---
+++ copy-recursive-Folder.ps1
@@ -0,0 +1,8 @@
+﻿
+$OriginPath = "C:\TEST"
+$CopyPath = "C:\FULL\TESTFULL_$(get-date -f yy-MM-dd--HH.mm.ss)"
+
+
+Copy-Item $OriginPath -Filter * -Destination $CopyPath -Recurse
+
+#Set up task scheduler to run automagicly https://community.spiceworks.com/how_to/17736-run-powershell-scripts-from-task-scheduler
